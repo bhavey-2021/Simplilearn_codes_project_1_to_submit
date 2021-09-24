6c77670345d4a383f41b91144442d9f71b419b53
bhavey malhotra <bhavey.m@gmail.com>
2021-09-24 16:42:34 +0530

    second task

--- src/locker/back/MainMenu.java
+++ src/locker/back/MainMenu.java
@@ -86,5 +86,124 @@ public class MainMenu {
 	    
 	    
 }
-	   
+	    public void viewData()
+	    {
+
+			File folder= new File("C:\\Eclipse_Java\\PhaseOneProject");
+
+	    	 
+	        String[] file1 = folder.list();
+	      List<String> files=  Arrays.asList(file1);
+	        Collections.sort(files);
+	 
+	        for (String file : files)
+	        {
+	            System.out.println(file);
+	        }
+	    }
+	    public void registration() throws IOException
+	    {
+	    	System.out.println("Enter your username");
+	    	  u_N = sc.next();
+	    	 String un=u_N;
+	    	 u_N = u_N+".txt";
+	    	
+	    	  try (BufferedReader r1 = new BufferedReader(new FileReader("userpass.txt"))) {
+	  			String st;
+	  			  while ((st = r1.readLine()) != null)
+	  			    if(st.equals(un))
+	  			    {
+	  			    	System.out.println("Username already taken,try another username");
+	  			    	display();
+	  			    
+	  			    }
+	  			  
+	  		}
+	    	 
+	  
+	    		File myFile=new File(u_N);
+
+	         System.out.println("You are adding a user named: " + un);
+	        	
+	 			try {	
+	 		      if (myFile.createNewFile()) {
+	 		    	  System.out.println("File created: " + u_N);
+	 		    	
+	 		    	  
+	 		      } else {
+	 		        System.out.println("This User Already Exits, no need to add another");
+	 		        display();
+	 		      }
+	 		}catch (IOException e){
+	 			System.out.println(e);
+	 		}
+	 			System.out.println("Enter your password");
+		    	 String passWord = sc.next();	
+		    	 
+		    	   
+		            try {
+		                FileWriter fileWriter = new FileWriter("userpass.txt",true);
+		                fileWriter.write(un+"\n");
+		               
+		                fileWriter.write(passWord+"\n");
+		                System.out.println("Registration Successful");
+		             fileWriter.close();
+		            } catch (IOException e) {
+		                e.printStackTrace();
+		            }
+
+	    }
+	    public void login() throws IOException
+	    {
+	    	System.out.println("Enter your username");
+	     	u_n=sc.next();
+	   unn=u_n;
+	    u_n =u_n+".txt";
+		
+
+		File myFile = new File("C:\\Eclipse_Java\\PhaseOneProject");
+
+		
+		String[] flist = myFile.list();
+		int flag = 0;
+		if (flist == null) {
+			System.out.println("Database is empty");
+		}
+		else {
+
+			// Linear search in the array
+			for (int i = 0; i < flist.length; i++) {
+				String filename = flist[i];
+				if (filename.equals(u_n)) {
+					System.out.println("Enter password");
+					 String	password=sc.next();
+					  BufferedReader br = new BufferedReader(new FileReader("userpass.txt"));
+					  
+					  String st;
+					  while ((st = br.readLine()) != null)
+					    if(st.equals(unn))
+					    {
+					    	st=br.readLine();
+					    	if(st.equals(password))
+					    	{
+					    		System.out.println("Login Successful");
+					    	UserMenu op1=new UserMenu(unn);
+					    		
+					    	}
+					    	else
+					    		System.out.println("Invalid Credentials");
+					    	
+					    }
+					flag = 1;
+
+				   
+				}
+			}
+		}
+
+		if (flag == 0) {
+			System.out.println("User not found,try again");
+			}
+	    }
+  
    }
